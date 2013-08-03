9ecde0e311163bfefa2665d3461a82a6cc123889
Zenith <fio_244@yahoo.com>
2013-08-04 00:16:31 +0300

    Socket::InitSockets must throw. TODO: later, I should change it so it returns void.

--- FileTransferApp/Socket.cpp
+++ FileTransferApp/Socket.cpp
@@ -1,4 +1,5 @@
 #include "General.h"
+#include "Exception.h"
 #include "Socket.h"
 
 extern BOOL bOrderEnd;
@@ -17,10 +18,11 @@ Socket::~Socket(void)
 int Socket::InitSockets(void)
 {
 	WSADATA wsaData;
-	int Result = WSAStartup(MAKEWORD(2,2), &wsaData);
-	if (Result != 0)
+	int result = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if (result != 0)
 	{
-		return Result;
+		//return Result;
+		THROW(WindowsException, result);
 	}
 
 	return 0;
